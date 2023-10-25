9c0256ae5d22b78a8a21be16d06667cec7c49d5f
Antoine Clarman <antoinereims28@gmail.com>
2023-10-25 19:29:49 +0200

    chore: add swagger

--- src/achievements/achievements.controller.ts
+++ src/achievements/achievements.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { AchievementsService } from './achievements.service';
 import { CreateAchievementDto } from './dto/create-achievement.dto';
 import { UpdateAchievementDto } from './dto/update-achievement.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('achievements')
 @Controller('achievements')
 export class AchievementsController {
   constructor(private readonly achievementsService: AchievementsService) {}
